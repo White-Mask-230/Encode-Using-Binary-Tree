9ec01e693da8a5fa7b7d5e4593def44ae4e762fe
Lucas Varela Correa <153003049+Lucas-Varela-Correa@users.noreply.github.com>
2024-06-15 03:13:13 +0200

    Solve bug that the letter repeat numbers

--- __init__.py
+++ __init__.py
@@ -1,13 +1,9 @@
 import random
 
-def create_dictionary_of_keys(symbols=None):
-    """ unsupported numbers """
-
+def create_dictionary_of_keys():
     max_number = random.randint(150, 300)
     prime_numbers = []
-
-    if not symbols:
-        symbols = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm']
+    symbols = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm']
 
     for i in range(2, max_number):
         z = 0
@@ -20,32 +16,41 @@ def create_dictionary_of_keys(symbols=None):
         if z == 0:
             prime_numbers.append(i)
         
-    dictionary_of_keys = {"layer 0" : {symbols[0] : random.choice(prime_numbers)}, "layer 1" : {}}
-        
+    dictionary_of_keys = {"layer 0": {symbols[0]: random.choice(prime_numbers)}, "layer 1": {}}
+    used_values = {list(dictionary_of_keys["layer 0"].values())[0]}  # Set to keep track of used values
+
     del symbols[0]
     
     layer = 1
-    id_previous_layer = 0 # Its function is to select the upstream element that must be added two other elements
-    counter = 0 # Its function is to ensure that only two elements are assigned to the upstream element.
-    i = 1 # Its function is to ensure that an empty layer is not created
+    id_previous_layer = 0  # Its function is to select the upstream element that must be added two other elements
+    counter = 0  # Its function is to ensure that only two elements are assigned to the upstream element.
+    i = 1  # Its function is to ensure that an empty layer is not created
 
     for symbol in symbols:        
         random_number = random.choice(prime_numbers)
-        value_1_upstream = list(dictionary_of_keys[f'layer {id_previous_layer}'].values())[id_previous_layer] 
+        value_1_upstream = list(dictionary_of_keys[f'layer {id_previous_layer}'].values())[id_previous_layer]
 
-        dictionary_of_keys[f'layer {layer}'][symbol] = value_1_upstream * random_number
+        new_value = value_1_upstream * random_number
+        
+        # Ensure new_value is unique
+        while new_value in used_values:
+            random_number = random.choice(prime_numbers)
+            new_value = value_1_upstream * random_number
+
+        dictionary_of_keys[f'layer {layer}'][symbol] = new_value
+        used_values.add(new_value)
 
         if len(list(dictionary_of_keys[f'layer {layer}'].values())) == 2 ** layer:
-            # a new layer is created
+            # A new layer is created
             
-            if len(symbols) == i: # prevents an empty layer from being created
+            if len(symbols) == i:  # Prevents an empty layer from being created
                 break
 
             layer += 1
             id_previous_layer = 0
             dictionary_of_keys[f'layer {layer}'] = {}
         else: 
-            # checks if the upstream element needs to be changed
+            # Checks if the upstream element needs to be changed
             
             if counter == 2:
                 id_previous_layer += 1
@@ -55,8 +60,6 @@ def create_dictionary_of_keys(symbols=None):
                 counter += 1
 
         i += 1
-    
-    #BUG Two letters can have the same value, which means that when one is deciphered, it appears as many times as the other should appear.
 
     return dictionary_of_keys
 
@@ -105,7 +108,7 @@ def decode(text: list[str], dictionary_of_keys):
     return decode_text
 
 def test():
-    text = ['hello my name is kopo', 'hello kopo', 'can you give a onion kopo', 'of course I will do that']
+    text = ['hello my name is kopo', 'hello kopo', 'can you give a onion kopo', 'of course i will do that']
 
     dictionary_of_keys = create_dictionary_of_keys()
 
@@ -125,21 +128,4 @@ def test():
         return False
 
 if __name__ == '__main__':
-    #print(test())
-
-    dictionary_of_keys = create_dictionary_of_keys()
-
-    encode_text = encode(
-        text=['hello my name is kopo', 'hello kopo', 'can you give a onion kopo', 'of course I will do that'],
-        dictionary_of_keys=dictionary_of_keys
-    )
-
-    print(encode_text)
-
-    decode_text = decode(
-        text=encode_text,
-        dictionary_of_keys=dictionary_of_keys
-    )
-
-    print(dictionary_of_keys)
-    print(decode_text)
+    print(test())
